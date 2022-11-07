e0c993604208ce3330cef59e00c5d7f389b62d13
evandor <evandor@gmail.com>
2022-11-07 17:35:13 +0100

    TAB-95

---
+++ src/pages/DebugPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="t in _.filter(urls, u => u.length> 1000)">
+    {{t.length}}: {{ t }}<br><br>
+    {{encoded(t)}}<br><br>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import {useRouter} from "vue-router";
+import {useFeatureTogglesStore} from "stores/featureTogglesStore";
+import {useTabsStore} from "stores/tabsStore";
+import {ref, watchEffect} from "vue"
+import _ from "lodash"
+
+const router = useRouter()
+const featureToggles = useFeatureTogglesStore()
+const tabsStore = useTabsStore()
+
+if (!featureToggles.debugEnabled) {
+  router.push("/")
+}
+
+const urls = ref<string[]>([])
+
+watchEffect(() =>
+  _.map([...tabsStore.tabsets.values()], ts => {
+    _.forEach(ts.tabs, tab => {
+      urls.value.push(tab?.chromeTab?.url || 'unknown')
+    })
+  })
+)
+
+const encoded = (t:string) => btoa(t)
+
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -51,6 +51,11 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/SearchPage.vue') }],
   },
   {
+    path: '/debug',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/DebugPage.vue') }],
+  },
+  {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
   },
