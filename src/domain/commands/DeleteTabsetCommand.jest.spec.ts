50c02d7e442a2474b157b0b886455dfcdcc4411b
evandor <evandor@gmail.com>
2023-01-30 08:00:52 +0100

    more tests

---
+++ src/domain/commands/DeleteTabsetCommand.jest.spec.ts
@@ -0,0 +1,46 @@
+import {beforeEach, describe, expect, it, jest} from "@jest/globals";
+import {chrome} from "jest-chrome";
+import {createPinia, setActivePinia} from "pinia";
+import {CreateTabsetCommand} from "src/domain/commands/CreateTabsetCommand";
+import "fake-indexeddb/auto"
+import IndexedDbPersistenceService from "src/services/IndexedDbPersistenceService";
+import {INDEX_DB_VERSION} from "boot/constants";
+import {useJestHelper} from "src/domain/JestHelper";
+import LoggingService from "src/services/LoggingService";
+import ChromeApi from "src/services/ChromeApi";
+import {useTabsStore} from "src/stores/tabsStore";
+import {useSearchStore} from "src/stores/searchStore";
+import {DeleteTabsetCommand} from "src/domain/commands/DeleteTabsetCommand";
+
+describe('CreateTabsetCommand', () => {
+
+  jest.setTimeout(10000)
+
+  beforeEach(async () => {
+    setActivePinia(createPinia())
+    await LoggingService.init()
+    await useSearchStore().init()
+    const request = indexedDB.open('db', INDEX_DB_VERSION);
+    request.onupgradeneeded = async function () {
+      await useJestHelper().dbInit(request)
+    }
+    await IndexedDbPersistenceService.init()
+    process.env.MODE = "bex"
+    chrome.tabs.query.mockImplementation(async (o: object) => [])
+  })
+
+  it('deletes new empty tabset', async () => {
+    const createTsCmd = new CreateTabsetCommand('emptyTabsetId', [])
+    await createTsCmd.execute()
+    const tabsets = useTabsStore().tabsets
+    expect(tabsets.size).toBe(1)
+    const ts = tabsets.values().next().value
+
+    const deleteTsCmd = new DeleteTabsetCommand(ts.id)
+    const res: any = await deleteTsCmd.execute()
+    console.log("res", res)
+    expect(res.message).toBe("Tabset was deleted")
+    expect(res.undoCommand).not.toBe(null)  })
+
+
+})
