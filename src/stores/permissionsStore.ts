a8c2e028f6244b43d6fb9af30b46e1a1985a9c68
Carsten Gräf <evandor@gmail.com>
2023-09-25 08:19:02 +0200

    Revert "Tab 414 alt sidepanel"

--- src/stores/permissionsStore.ts
+++ src/stores/permissionsStore.ts
@@ -1,7 +1,7 @@
 // !== MIT
 import {defineStore} from 'pinia';
-import {computed, ref} from "vue";
-import {useQuasar} from "quasar";
+import {computed, ref, watchEffect} from "vue";
+import {Notify, useQuasar} from "quasar";
 import {FeatureIdent, FeatureType} from "src/models/AppFeature";
 import {useSuggestionsStore} from "src/stores/suggestionsStore";
 import {StaticSuggestionIdent, Suggestion} from "src/models/Suggestion";
@@ -9,6 +9,7 @@ import {CreateSpecialTabsetCommand, SpecialTabsetIdent} from "src/domain/tabsets
 import {TabsetType} from "src/models/Tabset";
 import {useCommandExecutor} from "src/services/CommandExecutor";
 import {AppFeatures} from "src/models/AppFeatures";
+import {useDB} from "src/services/usePersistenceService";
 import {useUtils} from "src/services/Utils";
 import {useTabsetService} from "src/services/TabsetService2";
 import PersistenceService from "src/services/PersistenceService";
@@ -46,7 +47,7 @@ export const usePermissionsStore = defineStore('permissions', () => {
 
         if (chrome) { // issues in vitest where chrome is not defined
             // @ts-ignore
-            permissions.value = await browser.permissions.getAll()
+            permissions.value = await chrome.permissions.getAll()
             if (permissions.value) {
                 grantedOptionalPermissions.value = permissions.value.permissions ? permissions.value.permissions : []
                 grantedOptionalOrigins.value = permissions.value.origins ? permissions.value.origins : []
@@ -72,28 +73,28 @@ export const usePermissionsStore = defineStore('permissions', () => {
 
     async function grantPermission(permission: string): Promise<boolean> {
         // @ts-ignore
-        const granted: boolean = await browser.permissions.request({permissions: [permission]})
+        const granted: boolean = await chrome.permissions.request({permissions: [permission]})
         return load()
             .then(() => Promise.resolve(granted))
     }
 
     async function grantAllOrigins(): Promise<boolean> {
         // @ts-ignore
-        const granted: boolean = await browser.permissions.request({origins: ["*://*/*", "<all_urls>"]})
+        const granted: boolean = await chrome.permissions.request({origins: ["*://*/*", "<all_urls>"]})
         return load()
             .then(() => Promise.resolve(granted))
     }
 
     async function revokePermission(permission: string): Promise<void> {
         // @ts-ignore
-        await browser.permissions.remove({permissions: [permission]})
+        await chrome.permissions.remove({permissions: [permission]})
         await load()
         return Promise.resolve()
     }
 
     async function revokeAllOrigins(): Promise<void> {
         // @ts-ignore
-        await browser.permissions.remove({origins: ["*://*/*", "<all_urls>"]})
+        await chrome.permissions.remove({origins: ["*://*/*", "<all_urls>"]})
         await load()
         return Promise.resolve()
     }
