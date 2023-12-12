900799a7e2f95aa31138518dabeabbe35cfca965
Carsten Gräf <evandor@gmail.com>
2023-12-12 15:36:20 +0100

    TAB-453 isomorphic-git POC

--- src/services/persistence/GitPersistentService.ts
+++ src/services/persistence/GitPersistentService.ts
@@ -37,6 +37,19 @@ async function createDir(...segments: string[]) {
   }
 }
 
+async function push() {
+  let pushResult = await git.push({
+    fs,
+    http,
+    dir: '/tutorial',
+    remote: 'origin',
+    corsProxy: 'https://cors.isomorphic-git.org',
+    ref: 'main',
+    onAuth: () => ({username: process.env.GITHUB_TOKEN}),
+  })
+  console.log(pushResult)
+}
+
 class GitPersistenceService implements PersistenceService {
   private db: IDBPDatabase = null as unknown as IDBPDatabase
 
@@ -72,165 +85,8 @@ class GitPersistenceService implements PersistenceService {
       depth: 10
     });
 
-    await pfs.readdir(dir);
+   // await pfs.readdir(dir);
 
-    const log = await git.log({fs, dir})
-    console.log("git log", log)
-
-    const status = await git.status({fs, dir, filepath: 'README.md'})
-    console.log("status", status)
-  }
-
-  addGroup(group: chrome.tabGroups.TabGroup): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  addMessage(msg: Message): void {
-  }
-
-  addNotification(notification: Notification): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  addSpace(space: Space): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  addSuggestion(suggestion: Suggestion): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  addWindow(window: Window): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  cleanUpContent(): Promise<SearchDoc[]> {
-    return Promise.resolve([]);
-  }
-
-  cleanUpRequests(): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  cleanUpTabsets(): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  cleanUpThumbnails(): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  clear(name: string): any {
-  }
-
-  compactDb(): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  deleteBlob(tabId: string, elementId: string): void {
-  }
-
-  deleteContent(url: string): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  deleteGroupByTitle(title: string): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  deleteMHtml(id: string): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  deleteSpace(spaceId: string): void {
-  }
-
-  deleteTabset(tabsetId: string): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  deleteThumbnail(url: string): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  getActiveFeatures(): Promise<string[]> {
-    return Promise.resolve([]);
-  }
-
-  getBlobs(type: BlobType): Promise<any[]> {
-    return Promise.resolve([]);
-  }
-
-  getBlobsForTab(tabId: string): Promise<SavedBlob[]> {
-    return Promise.resolve([]);
-  }
-
-  getContent(url: string): Promise<object> {
-    return Promise.resolve({});
-  }
-
-  getContents(): Promise<any[]> {
-    return Promise.resolve([]);
-  }
-
-  getGroups(): Promise<chrome.tabGroups.TabGroup[]> {
-    return Promise.resolve([]);
-  }
-
-  getLinks(url: string): Promise<object> {
-    return Promise.resolve({});
-  }
-
-  getMHtml(url: string): Promise<object> {
-    return Promise.resolve({});
-  }
-
-  getMHtmlInline(url: string): Promise<object> {
-    return Promise.resolve({});
-  }
-
-  getMHtmls(): Promise<MHtml[]> {
-    return Promise.resolve([]);
-  }
-
-  getMessages(): Promise<Message[]> {
-    return Promise.resolve([]);
-  }
-
-  getMetaLinks(url: string): Promise<object> {
-    return Promise.resolve({});
-  }
-
-  getNotifications(onlyNew: boolean): Promise<Notification[]> {
-    return Promise.resolve([]);
-  }
-
-  getRequest(url: string): Promise<string> {
-    return Promise.resolve("");
-  }
-
-  getSuggestions(): Promise<Suggestion[]> {
-    return Promise.resolve([]);
-  }
-
-  getThumbnail(url: string): Promise<string> {
-    return Promise.resolve("");
-  }
-
-  getWindow(windowId: number): Promise<Window | undefined> {
-    return Promise.resolve(undefined);
-  }
-
-  getWindows(): Promise<Window[]> {
-    return Promise.resolve([]);
-  }
-
-  loadCategories(): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  loadSpaces(): Promise<any> {
-    return Promise.resolve(undefined);
   }
 
   async loadTabsets(): Promise<void> {
@@ -274,47 +130,6 @@ class GitPersistenceService implements PersistenceService {
     return Promise.resolve(undefined);
   }
 
-  notificationRead(notificationId: string): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  reloadTabset(tabsetId: string): void {
-  }
-
-  removeSuggestion(id: string): any {
-  }
-
-  removeWindow(windowId: number): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  saveActiveFeatures(val: string[]): any {
-  }
-
-  saveBlob(id: string, url: string, data: Blob, type: BlobType, remark: string | undefined): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  saveContent(tab: Tab, text: string, metas: object, title: string, tabsetIds: string[]): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  saveLinks(url: string, links: any): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  saveMHtml(tab: Tab, mhtml: Blob): Promise<string> {
-    return Promise.resolve("");
-  }
-
-  saveMetaLinks(url: string, metaLinks: MetaLink[]): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
-  saveRequest(url: string, requestInfo: RequestInfo): Promise<void> {
-    return Promise.resolve(undefined);
-  }
-
   async saveTabset(tabset: Tabset): Promise<any> {
     // assuming new
     const tabsetFilename = `tabsets/${tabset.id}`
@@ -323,13 +138,14 @@ class GitPersistenceService implements PersistenceService {
     let strippedTabset = JSON.parse(JSON.stringify(tabset));
     const tabs = strippedTabset.tabs as Tab[]
     delete strippedTabset['tabs']
-    await pfs.writeFile(`${dir}/tabsets/${tabset.id}/tabset.json`, JSON.stringify(strippedTabset), 'utf8')
+    await pfs.writeFile(`${dir}/tabsets/${tabset.id}/tabset.json`,
+      JSON.stringify(strippedTabset, null, 2), 'utf8')
     //await git.status({fs, dir, filepath: 'newfile.txt'})
     await git.add({fs, dir, filepath: tabsetFilename})
 
     // add / update tabs
     for(const t of tabs) {
-      await pfs.writeFile(`${dir}/tabsets/${tabset.id}/tabs/${t.id}`, JSON.stringify(t), 'utf8')
+      await pfs.writeFile(`${dir}/tabsets/${tabset.id}/tabs/${t.id}`, JSON.stringify(t, null, 2), 'utf8')
       //await git.status({fs, dir, filepath: 'newfile.txt'})
       await git.add({fs, dir, filepath: tabsetFilename + "/tabs"})
     }
@@ -345,46 +161,232 @@ class GitPersistenceService implements PersistenceService {
     })
 
     console.log(sha)
-
-    let pushResult = await git.push({
-      fs,
-      http,
-      dir: '/tutorial',
-      remote: 'origin',
-      corsProxy: 'https://cors.isomorphic-git.org',
-      ref: 'main',
-      onAuth: () => ({username: process.env.GITHUB_TOKEN}),
-    })
-    console.log(pushResult)
-
+    await push();
     return Promise.resolve(undefined);
   }
 
-  saveThumbnail(tab: chrome.tabs.Tab, thumbnail: string): Promise<void> {
+  async deleteTabset(tabsetId: string): Promise<any> {
+    await pfs.rmdir(`${dir}/tabsets/${tabset.id}`)
+    await push();
     return Promise.resolve(undefined);
+  }
+
+
+  addGroup(group: chrome.tabGroups.TabGroup): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  addMessage(msg: Message): void {
+  }
+
+  addNotification(notification: Notification): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  addSpace(space: Space): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  addSuggestion(suggestion: Suggestion): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  addWindow(window: Window): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  cleanUpContent(): Promise<SearchDoc[]> {
+    return Promise.reject([]);
+  }
+
+  cleanUpRequests(): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  cleanUpTabsets(): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  cleanUpThumbnails(): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  clear(name: string): any {
+  }
+
+  compactDb(): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  deleteBlob(tabId: string, elementId: string): void {
+  }
+
+  deleteContent(url: string): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  deleteGroupByTitle(title: string): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  deleteMHtml(id: string): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  deleteSpace(spaceId: string): void {
+  }
+
+  deleteThumbnail(url: string): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  getActiveFeatures(): Promise<string[]> {
+    return Promise.reject([]);
+  }
+
+  getBlobs(type: BlobType): Promise<any[]> {
+    return Promise.reject([]);
+  }
+
+  getBlobsForTab(tabId: string): Promise<SavedBlob[]> {
+    return Promise.reject([]);
+  }
+
+  getContent(url: string): Promise<object> {
+    return Promise.reject({});
+  }
+
+  getContents(): Promise<any[]> {
+    return Promise.reject([]);
+  }
+
+  getGroups(): Promise<chrome.tabGroups.TabGroup[]> {
+    return Promise.reject([]);
+  }
+
+  getLinks(url: string): Promise<object> {
+    return Promise.reject({});
+  }
+
+  getMHtml(url: string): Promise<object> {
+    return Promise.reject({});
+  }
+
+  getMHtmlInline(url: string): Promise<object> {
+    return Promise.reject({});
+  }
+
+  getMHtmls(): Promise<MHtml[]> {
+    return Promise.reject([]);
+  }
+
+  getMessages(): Promise<Message[]> {
+    return Promise.reject([]);
+  }
+
+  getMetaLinks(url: string): Promise<object> {
+    return Promise.reject({});
+  }
+
+  getNotifications(onlyNew: boolean): Promise<Notification[]> {
+    return Promise.reject([]);
+  }
+
+  getRequest(url: string): Promise<string> {
+    return Promise.reject("");
+  }
+
+  getSuggestions(): Promise<Suggestion[]> {
+    return Promise.reject([]);
+  }
+
+  getThumbnail(url: string): Promise<string> {
+    return Promise.reject("");
+  }
+
+  getWindow(windowId: number): Promise<Window | undefined> {
+    return Promise.reject(undefined);
+  }
+
+  getWindows(): Promise<Window[]> {
+    return Promise.reject([]);
+  }
+
+  loadCategories(): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  loadSpaces(): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+
+  notificationRead(notificationId: string): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  reloadTabset(tabsetId: string): void {
+  }
+
+  removeSuggestion(id: string): any {
+  }
+
+  removeWindow(windowId: number): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  saveActiveFeatures(val: string[]): any {
+  }
+
+  saveBlob(id: string, url: string, data: Blob, type: BlobType, remark: string | undefined): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  saveContent(tab: Tab, text: string, metas: object, title: string, tabsetIds: string[]): Promise<any> {
+    return Promise.reject(undefined);
+  }
+
+  saveLinks(url: string, links: any): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  saveMHtml(tab: Tab, mhtml: Blob): Promise<string> {
+    return Promise.reject("");
+  }
+
+  saveMetaLinks(url: string, metaLinks: MetaLink[]): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  saveRequest(url: string, requestInfo: RequestInfo): Promise<void> {
+    return Promise.reject(undefined);
+  }
+
+  saveThumbnail(tab: chrome.tabs.Tab, thumbnail: string): Promise<void> {
+    return Promise.reject(undefined);
   }
 
   setSuggestionState(id: string, state: SuggestionState): any {
   }
 
   updateContent(url: string): Promise<object> {
-    return Promise.resolve({});
+    return Promise.reject({});
   }
 
   updateGroup(group: chrome.tabGroups.TabGroup): Promise<any> {
-    return Promise.resolve(undefined);
+    return Promise.reject(undefined);
   }
 
   updateThumbnail(url: string): Promise<void> {
-    return Promise.resolve(undefined);
+    return Promise.reject(undefined);
   }
 
   updateWindow(window: Window): Promise<void> {
-    return Promise.resolve(undefined);
+    return Promise.reject(undefined);
   }
 
   upsertWindow(window: Window, windowName: string, screenLabel: string | undefined): Promise<void> {
-    return Promise.resolve(undefined);
+    return Promise.reject(undefined);
   }
 
 }
