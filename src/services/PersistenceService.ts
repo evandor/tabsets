1c62c35f9b8ea09c77d5f4cda8bf787a6071265e
Carsten Gräf <evandor@gmail.com>
2023-12-14 16:32:50 +0100

    TAB-456 use git as backup / sync

--- src/services/PersistenceService.ts
+++ src/services/PersistenceService.ts
@@ -9,6 +9,7 @@ import {MetaLink} from "src/models/MetaLink";
 import {Window} from "src/models/Window";
 import {RequestInfo} from "src/models/RequestInfo";
 import {BlobType, SavedBlob} from "src/models/SavedBlob";
+import {Message} from "src/models/Message";
 
 interface PersistenceService {
 
