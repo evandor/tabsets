97d4afb26050e2d44e3346522637d2387f811f2e
Carsten Gräf <evandor@gmail.com>
2024-03-07 08:17:07 +0100

    TAB-143 improvements

--- src/components/buttons/DialogButton.vue
+++ src/components/buttons/DialogButton.vue
@@ -1,14 +1,14 @@
 <template>
   <q-btn
-      :type="props.type"
-      padding="xs lg"
-         :disable="props.disable"
-         @click.stop="emits('wasClicked')"
-         align="around"
-         size="0.9rem"
-         data-testid="addTabsetSubmitBtn"
-         :color="props.color"
-         :label="props.label"/>
+    :type="props.type"
+    padding="xs lg"
+    :disable="props.disable"
+    @click="emits('wasClicked')"
+    align="around"
+    size="0.9rem"
+    data-testid="addTabsetSubmitBtn"
+    :color="props.color"
+    :label="props.label"/>
 </template>
 
 <script lang="ts" setup>
