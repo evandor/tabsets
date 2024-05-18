7f48dd0553fd59a2ae28e018ca7228c9a66e6231
Carsten Gräf <evandor@gmail.com>
2024-05-18 08:30:09 +0200

    work on features submodule, docs

---
+++ src/pages/FeaturePage.vue
@@ -0,0 +1,25 @@
+<template>
+  hi2: {{count}}<br>
+  hi2: {{counterStore.count}}<br>
+  hi2: {{useCounterStore().count}}<br>
+
+  <q-btn label="inc" @click="counterStore.increment()" />
+
+</template>
+
+<script lang="ts" setup>
+
+import {ref, watchEffect} from "vue";
+import {useCounterStore} from "stores/counterStore";
+
+const count = ref(1)
+
+const counterStore = useCounterStore()
+
+watchEffect(() => {
+  console.log("hier")
+  count.value = counterStore.count
+})
+
+
+</script>
