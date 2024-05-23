7455390b81deb74a10a2d82e9eddae3df1db37a1
Carsten Gräf <evandor@gmail.com>
2024-05-23 07:40:34 +0200

    submodules refactoring

--- src/stores/permissionsStore.ts
+++ src/stores/permissionsStore.ts
@@ -107,66 +107,6 @@ export const usePermissionsStore = defineStore('permissions', () => {
   const featuresCount = computed(() => (): number =>
     activeFeatures.value.length)
 
-  const activateFeature = computed(() => {
-    return (feature: string): void => {
-      if (storage && activeFeatures.value.indexOf(feature) < 0) {
-        activeFeatures.value.push(feature)
-        storage.saveActiveFeatures(activeFeatures.value)
-
-        if (FeatureIdent.SPACES.toLowerCase() === feature) {
-          useSuggestionsStore().inactivateSuggestion(Suggestion.getStaticSuggestion(StaticSuggestionIdent.TRY_SPACES_FEATURE))
-        }
-        if (FeatureIdent.BACKUP.toLowerCase() === feature) {
-          useCommandExecutor().executeFromUi(new CreateSpecialTabsetCommand(SpecialTabsetIdent.BACKUP, TabsetType.SPECIAL))
-        }
-        if (FeatureIdent.HELP.toLowerCase() === feature) {
-          useCommandExecutor().executeFromUi(new CreateSpecialTabsetCommand(SpecialTabsetIdent.HELP, TabsetType.SPECIAL))
-        } else if (FeatureIdent.IGNORE.toLowerCase() === feature) {
-          //useSuggestionsStore().removeSuggestion(StaticSuggestionIdent.TRY_TAB_DETAILS_FEATURE)
-          useCommandExecutor().executeFromUi(new CreateSpecialTabsetCommand(SpecialTabsetIdent.IGNORE, TabsetType.SPECIAL))
-        }
-        sendMsg('feature-activated', {feature: feature})
-      }
-    }
-  })
-
-  function deactivateRecursive(feature: string) {
-    console.log("deactivate recursive: ", feature)
-    const deactivatedIdent = feature.toUpperCase() as FeatureIdent
-    const appFeature = new AppFeatures().getFeature(deactivatedIdent)
-
-    //console.log("deactivating normal feature", feature)
-    const index = activeFeatures.value.indexOf(feature)
-    if (index >= 0) {
-      if (FeatureIdent.HELP.toLowerCase() === feature) {
-        useTabsetService().deleteTabset("HELP")
-        // Notify.create({
-        //     color: 'warning',
-        //     message: "The Help pages have been deleted"
-        // })
-      }
-      activeFeatures.value.splice(index, 1)
-      storage.saveActiveFeatures(activeFeatures.value)
-      sendMsg('feature-deactivated', {feature: feature})
-      new AppFeatures().getFeatures().forEach(f => {
-        if (f.requires.findIndex((r: string) => {
-          return r === deactivatedIdent.toString()
-        }) >= 0) {
-          console.log("need to deactivate as well:", f)
-          deactivateRecursive(f.ident.toLowerCase())
-        }
-      })
-      //console.log("deactivated", feature, activeFeatures.value)
-    }
-
-  }
-
-  const deactivateFeature = computed(() => {
-    return (feature: string): void => {
-      //console.log("deactivating", feature)
-      deactivateRecursive(feature)
-    }
-  })
 
   function addActivateFeature(feature: string): boolean {
     if (activeFeatures.value.indexOf(feature) < 0) {
