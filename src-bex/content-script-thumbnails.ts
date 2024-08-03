0aa45149253132fe1db67031274446f3e9d7aed2
Carsten Gräf <evandor@gmail.com>
2024-08-03 16:48:20 +0200

    alignment with bibbly

--- src-bex/content-script-thumbnails.ts
+++ src-bex/content-script-thumbnails.ts
@@ -1,9 +1,10 @@
+// Hooks added here have a bridge allowing communication between the BEX Content Script and the Quasar Application.
+// More info: https://quasar.dev/quasar-cli/developing-browser-extensions/content-hooks
 // @ts-ignore
 import {bexContent} from 'quasar/wrappers'
 
 export default bexContent((bridge: any) => {
 
-  console.log("tabsets: initializing content script for thumbnails")
 
   // @ts-ignore
   if (window.contentScriptThumbnailsAlredyCalled) {
@@ -14,11 +15,21 @@ export default bexContent((bridge: any) => {
   // @ts-ignore
   window.contentScriptThumbnailsAlredyCalled  = true
 
-  chrome.runtime.sendMessage({msg: "captureThumbnail"}, function (response) {
-    console.log("tabsets: created thumbnail for tabsets")
-    if (chrome.runtime.lastError) {
-      console.warn("got runtime error", chrome.runtime.lastError)
+  console.log("tabsets: initializing content script for thumbnails")
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("got request!!!", request)
+    if (request === 'getContent') {
+      console.log("tabsets: received message for content")
+      //sendResponse({content: document.documentElement.outerHTML});
+      sendResponse({
+        html: document.documentElement.outerHTML,
+        metas: getMetas(document)
+      });
+    } else {
+      sendResponse({content: "unknown request in content-scripts-thumbnails: " + request});
     }
-  });
+    return true
+  })
 
 })
--- src-bex/dom.ts
+++ src-bex/dom.ts
@@ -1,13 +1,115 @@
 // Hooks added here have a bridge allowing communication between the Web Page and the BEX Content Script.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/dom-hooks
 import {bexDom} from 'quasar/wrappers'
-import detectQuasar from "app/src-bex/dom/detect-quasar";
+import {diffWords} from "diff"
+import {useUtils} from "src/core/services/Utils";
+import {useRoute} from "vue-router";
+
+const {sendMsg} = useUtils()
+
+const route = useRoute()
 
 export default bexDom((bridge) => {
-    //console.log("bexDom", bridge)
-    //if (!useFeaturesStore().hasFeature(FeatureIdent.ANALYSE_TABS)) {
-    // bridge.on('initDomScripts', ({ data, respond }) => {
-    //   console.log("tabsets: got event initDomScripts")
-    // })
-   // detectQuasar(bridge)
+
+  document.addEventListener('focusin', function (e: FocusEvent) {
+    console.log('focusin!', e)
+    const target = e.target!
+    // console.log('focusin!', e.target)
+    // console.log('focusin!', e.target?.innerHTML)
+
+      // @ts-ignore
+    if (target.dataset.changedHtml) {
+      console.log("setting to changed HTML")
+      // @ts-ignore
+      target.innerHTML = target.dataset.changedHtml
+    } else {
+      // @ts-ignore
+      e.target.dataset.originalHtml = e.target.innerHTML
+    }
+    // @ts-ignore
+    console.log("set data to", e.target.dataset.originalHtml)
+  })
+
+  document.addEventListener('focusout', function (e: FocusEvent) {
+
+    const target = e.target!
+    console.log('focusout!', e)
+    console.log('focusout!', e.target)
+    // console.log('focusout!', e.target.innerHTML)
+
+    // @ts-ignore
+    //const diff = diffChars(target.dataset.originalHtml || '', target.innerHTML);
+    const diff = diffWords(target.dataset.originalHtml || '', target.innerHTML);
+    // const diff = execute(e.target.dataset.originalHtml || '', e.target.innerHTML);
+    console.log("diff", diff)
+
+    let fragment = document.createDocumentFragment();
+    let html = ''
+    diff.forEach((part: any) => {
+      if (part.added) {
+        const span = document.createElement('span');
+        span.style.backgroundColor = "lightgreen";
+        var doc = new DOMParser().parseFromString(part.value, "text/html");
+        span.innerHTML = part.value
+        fragment.appendChild(span);
+        html += span.outerHTML
+      } else if (part.removed) {
+        const span = document.createElement('span');
+        span.style.backgroundColor = "lightred";
+        span.style.textDecoration = "line-through"
+        let doc = new DOMParser().parseFromString(part.value, "text/html");
+        span.innerHTML = part.value
+        fragment.appendChild(span);
+        html += span.outerHTML
+
+      } else {
+        html += part.value
+      }
+
+
+      // const color = part.added ? 'green' :
+      //   part.removed ? 'red' : 'grey';
+      // const span = document.createElement('span');
+      // span.style.backgroundColor = color;
+      // //console.log("part.value", part.value)
+      //
+      // var doc = new DOMParser().parseFromString(part.value, "text/html");
+      // // span.appendChild(doc.innerHTML);
+      // span.innerHTML = part.value
+      // console.log("span", span)
+      // fragment.appendChild(span);
+      //html += span.outerHTML
+
+    });
+
+    console.log("parsing", html)
+    const domFromHtml = new DOMParser().parseFromString(html, "text/html");
+
+
+    // @ts-ignore
+    target.dataset.changedHtml = target.innerHTML
+    // @ts-ignore
+    target.innerHTML = domFromHtml.body.innerHTML
+
+    sendMsg('snapshot-edited', {
+      html: document.documentElement.innerHTML,
+      path: document.location.hash
+    })
+
+    //target.appendChild(fragment.textContent)
+    // document.body.appendChild(fragment)
+    //  document.body.insertAdjacentHTML(
+    //    "beforeend",
+    //    div.innerHTML
+    //  );
+
+  })
+  //alert("sendingsending")
+  // bridge.send('quasar.detect', {})
+  //   .then((answer: any) => {
+  //     console.log("answer", answer)
+  //   })
+  //   .catch((error: any) => {
+  //     console.log("error", error)
+  //   })
 })
