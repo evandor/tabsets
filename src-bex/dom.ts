b7605dc56e99ee56e2172c5c2c88f4cc8b1da3c1
Carsten Gräf <evandor@gmail.com>
2024-08-24 14:34:26 +0200

    fix subfolder of subfolder

--- src-bex/dom.ts
+++ src-bex/dom.ts
@@ -1,21 +1,7 @@
 // Hooks added here have a bridge allowing communication between the Web Page and the BEX Content Script.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/dom-hooks
 import {bexDom} from 'quasar/wrappers'
-import {useUtils} from "src/core/services/Utils";
-
-const {sendMsg} = useUtils()
 
 export default bexDom((bridge) => {
 
-  console.log("=====================")
-
-
-  //alert("sendingsending")
-  // bridge.send('quasar.detect', {})
-  //   .then((answer: any) => {
-  //     console.log("answer", answer)
-  //   })
-  //   .catch((error: any) => {
-  //     console.log("error", error)
-  //   })
 })
