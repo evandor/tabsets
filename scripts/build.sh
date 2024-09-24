bbc9ce122cfd8dc7fd4e1c327654c8ccac2a3232
Carsten Gräf <evandor@gmail.com>
2024-09-24 14:58:26 +0200

    updates after release

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,20 +8,24 @@ fi
 
 if [ "$1" = "chrome" ]; then
   manifest="manifest-chrome.json"
+  envfile=".dev"
 elif [ "$1" = "firefox" ]; then
   manifest="manifest-firefox.json"
+  envfile=".dev"
 elif [ "$1" = "edge" ]; then
   manifest="manifest-edge.json"
+  envfile=".edge.env"
 elif [ "$1" = "opera" ]; then
   manifest="manifest-opera.json"
+  envfile=".opera.dev"
 fi
 
-echo "using manifest src-bex/$manifest and .dev.env"
+echo "using manifest src-bex/$manifest and $envfile"
 mv src-bex/manifest.json src-bex/manifest.tmp
 mv .env .env.tmp
 
 cp src-bex/$manifest src-bex/manifest.json
-cp .dev.env .env
+cp $envfile .env
 
 quasar build -m bex
 
