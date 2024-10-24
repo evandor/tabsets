2021147eb3e85c7879eec567f75e3884c71cbb16
Carsten Gräf <evandor@gmail.com>
2024-10-24 18:29:35 +0200

    wip

--- src/boot/constants.ts
+++ src/boot/constants.ts
@@ -9,7 +9,7 @@ const MONITORING_PERIOD_IN_MINUTES = 60 // in prod: 60
 
 const SPECIAL_ID_FOR_NO_GROUP_ASSIGNED = "no_group_assigned_identifier"
 
-const STRIP_CHARS_IN_USER_INPUT = /[`@#$%^*=\[\]{};:"\\|<>\/~]/g;
+const STRIP_CHARS_IN_USER_INPUT = /[`@#$%^*=\[\]{};"\\|<>~]/g;
 const STRIP_CHARS_IN_COLOR_INPUT = /[`@$%^*=\[\]{};:"\\|<>\/~]/g;
 
 // const RELEASE_NOTES_URL = "https://us-central1-tabsets-backend-prd.cloudfunctions.net/app/share/preview/5d2cccf9-83ea-40be-bc84-37b03e38af1d"
