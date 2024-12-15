274d85337ecdb20b14675776b2f8749a098d7fa2
Carsten Gräf <evandor@gmail.com>
2024-12-15 11:09:12 +0100

    wip

--- src/services/TabService.ts
+++ src/services/TabService.ts
@@ -1,10 +1,9 @@
-import {Tab} from "src/tabsets/models/Tab";
-import {useWindowsStore} from "src/windows/stores/windowsStore";
-import {useTabsStore2} from "src/tabsets/stores/tabsStore2";
-import {useUtils} from "src/core/services/Utils";
-import {useContentStore} from "src/content/stores/contentStore";
+import { Tab } from 'src/tabsets/models/Tab'
+import { useWindowsStore } from 'src/windows/stores/windowsStore'
+import { useTabsStore2 } from 'src/tabsets/stores/tabsStore2'
+import { useUtils } from 'src/core/services/Utils'
 
-const {inBexMode} = useUtils()
+const { inBexMode } = useUtils()
 
 class TabService {
 
@@ -13,13 +12,13 @@ class TabService {
       return false
     }
     const windowId = useWindowsStore().currentChromeWindow?.id || 0
-    const currentChromeTab:chrome.tabs.Tab | undefined = useTabsStore2().getCurrentChromeTab(windowId) || useTabsStore2().currentChromeTab
+    const currentChromeTab: chrome.tabs.Tab | undefined = useTabsStore2().getCurrentChromeTab(windowId) || useTabsStore2().currentChromeTab
     // console.log("checking current tab", currentChromeTab, tab.url, currentChromeTab.url === tab.url)
 
     // special urls
     //if (currentChromeTab?.url === "https://excalidraw.com/" && useContentStore().currentLocalStorage) {
-      console.warn("fix needed")
-      //return tab.id === useContentStore().currentLocalStorage['tabsetsTabId']
+    //  console.warn("fix needed")
+    //return tab.id === useContentStore().currentLocalStorage['tabsetsTabId']
     //}
 
     if (currentChromeTab?.url === tab.url) {
@@ -27,7 +26,7 @@ class TabService {
       return true
     }
     //console.log("checking", currentChromeTab.url, "/" + btoa(tab.url || ''), currentChromeTab.url?.indexOf("/" + btoa(tab.url || '')) )
-    if (currentChromeTab?.url && currentChromeTab.url?.indexOf("/" + btoa(tab.url || '')) >= 0) {
+    if (currentChromeTab?.url && currentChromeTab.url?.indexOf('/' + btoa(tab.url || '')) >= 0) {
       return true
     }
     return false
