d46d19c0957b0e5fe6e96b442ea595a587e423cf
Carsten Gräf <evandor@gmail.com>
2024-12-26 07:58:44 +0100

    further cleanup, test fix, prettier

--- src/domain/Types.ts
+++
@@ -1,4 +0,0 @@
-import { Tab } from 'src/tabsets/models/Tab'
-
-export type TabPredicate = (t: Tab) => boolean
-export type Predicate<T> = (t: T) => boolean
--- test/vitest/setup-file.ts
+++ test/vitest/setup-file.ts
@@ -1,6 +1,6 @@
 // // This file will be run before each test file
 import { INDEX_DB_VERSION } from 'src/boot/constants'
-import { useJestHelper } from 'src/domain/JestHelper'
+import { useJestHelper } from 'src/core/domain/JestHelper'
 import 'fake-indexeddb/auto'
 import { config } from '@vue/test-utils'
 
