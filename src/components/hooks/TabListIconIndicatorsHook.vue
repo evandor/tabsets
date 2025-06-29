a6f05f19c408d7e25c3fef631e708124c2cbd18b
Carsten Gräf <evandor@gmail.com>
2025-06-29 10:19:16 +0200

    Revert "starting pro variant in main branch"
    This reverts commit e90132cc278325bb3cac1cff45956a5e69f8e030.

--- src/components/hooks/TabListIconIndicatorsHook.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!--  <q-icon v-if="showResearchIndicator"-->
-  <!--    name="o_science" size="11px" color="primary" class="q-mr-xs">-->
-  <!--    <q-tooltip class="tooltip-small">This Source has associcated reasearch data</q-tooltip>-->
-  <!--  </q-icon>-->
-  <div></div>
-</template>
-
-<script lang="ts" setup></script>
--- src/layouts/SidePanelNoFooterLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <q-layout view="hHh lpR fFf">
-    <q-page-container style="max-width: 400px; margin: 0 auto">
-      <router-view />
-    </q-page-container>
-  </q-layout>
-</template>
-
-<script setup lang="ts"></script>
