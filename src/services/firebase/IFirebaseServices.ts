b73d152d164cd7b2e73807ad519c9245a103443e
Carsten Gräf <evandor@gmail.com>
2025-07-09 19:24:45 +0200

    tabsets-386 baby steps

--- src/services/firebase/IFirebaseServices.ts
+++ src/services/firebase/IFirebaseServices.ts
@@ -1,4 +1,5 @@
 import { Auth } from 'firebase/auth/web-extension'
+import { Database } from 'firebase/database'
 import { Firestore } from 'firebase/firestore'
 import { FirebaseStorage } from 'firebase/storage'
 
@@ -10,6 +11,8 @@ interface IFirebaseServices {
   getFirestore(): Firestore
 
   getStorage(): FirebaseStorage
+
+  getDatabase(): Database
 }
 
 export default IFirebaseServices
